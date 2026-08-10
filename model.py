e706a20b93c3844d3642c51ee8bed1c8f53a631c
aryan <71094167+sin-EASTER@users.noreply.github.com>
2026-08-11 01:56:18 +0530

    Step 38: pick_block_size

--- model.py
+++ model.py
@@ -343,8 +343,14 @@ def slice_train_and_val(data, split_idx):
     return (data[:split_idx], data[split_idx:])
     pass
 
-# Step 38 - pick_block_size (not yet solved)
-# TODO: implement
+# Step 38 - pick_block_size
+def pick_block_size(default_size):
+    """Return the context length (block_size) for training windows."""
+    # TODO: return an integer block size, at least 1, derived from default_size
+    if default_size<1 : 
+        return 1
+    return default_size
+    pass
 
 # Step 39 - slice_x_at_offset (not yet solved)
 # TODO: implement
