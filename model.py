a19eb3bbf4a930f3661af91bcc11fc68aa152145
aryan <71094167+sin-EASTER@users.noreply.github.com>
2026-08-11 01:42:18 +0530

    Step 37: slice_train_and_val

--- model.py
+++ model.py
@@ -336,8 +336,12 @@ def pick_split_point(n, train_frac):
 
     pass
 
-# Step 37 - slice_train_and_val (not yet solved)
-# TODO: implement
+# Step 37 - slice_train_and_val
+def slice_train_and_val(data, split_idx):
+    """Split a 1D token-id array into (train, val) at split_idx."""
+    # TODO: return (data[:split_idx], data[split_idx:])
+    return (data[:split_idx], data[split_idx:])
+    pass
 
 # Step 38 - pick_block_size (not yet solved)
 # TODO: implement
